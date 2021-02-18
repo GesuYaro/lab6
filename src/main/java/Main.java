6fcd735f6a437edc476a1008ee6c626b9bb6d304
Semyon Shagiev <shagiev.2002@mail.ru>
2021-02-18 15:42:07 +0300

    Test Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,26 +18,25 @@ public class Main {
         ArrayList<MusicBand> musicBandArrayList = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        CommandHandler.HistoryStorage historyStorage = new Console.CommandHandler.HistoryStorage();
         Console console = new Console(); //
         CommandHandler commandHandler = new CommandHandler(); //
         MusicBandFieldsReader musicBandFieldsReader = new MusicBandFieldsReader(reader);
         InteractiveModeReader interactiveModeReader = new InteractiveModeReader(musicBandFieldsReader, console);
         HashMap<String, Command> commands = new HashMap<>();
         ArrayListManager arrayListManager = new ArrayListManager(musicBandArrayList, LocalDate.now());
-        commands.put("help", new HelpCommand(console)); //не работает
-        commands.put("info", new InfoCommand(console, arrayListManager));
-        commands.put("show", new ShowCommand(console, arrayListManager));
-        commands.put("add", new AddCommand(console, arrayListManager, interactiveModeReader));
-        commands.put("update", new UpdateCommand(console,arrayListManager, interactiveModeReader));
-        commands.put("remove_by_id", new RemoveByIdCommand(arrayListManager));
-        commands.put("clear", new ClearCommand(arrayListManager));
-        commands.put("exit", new ExitCommand());
-        commands.put("insert_at", new InsertAtCommand(console, arrayListManager, interactiveModeReader));
-        commands.put("remove_last", new RemoveLastCommand(arrayListManager));
-        commands.put("history", new HistoryCommand(console, commandHandler)); //не работает
+//        commands.put("help", new HelpCommand(console)); //не работает
+        commands.put("info", new InfoCommand(writer, arrayListManager));
+//        commands.put("show", new ShowCommand(console, arrayListManager));
+//        commands.put("add", new AddCommand(console, arrayListManager, interactiveModeReader));
+//        commands.put("update", new UpdateCommand(console,arrayListManager, interactiveModeReader));
+//        commands.put("remove_by_id", new RemoveByIdCommand(arrayListManager));
+//        commands.put("clear", new ClearCommand(arrayListManager));
+//        commands.put("exit", new ExitCommand());
+//        commands.put("insert_at", new InsertAtCommand(console, arrayListManager, interactiveModeReader));
+//        commands.put("remove_last", new RemoveLastCommand(arrayListManager));
+        commands.put("history", new HistoryCommand(writer,historyStorage)); // должно начать работать
         commandHandler.setCommands(commands);
-        console.setCommandHandler(commandHandler);
-        console.setReader(reader);
         console.run();
     }
 }
